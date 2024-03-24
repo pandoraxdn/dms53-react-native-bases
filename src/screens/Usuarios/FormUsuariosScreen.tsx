eece5e25846cb33cd3f07ffe0088a0bac8d29047
pandoraxdn <pandoraxdn@protonmail.com>
2024-03-24 15:15:18 -0600

    add btn image form usuarios

--- src/screens/Usuarios/FormUsuariosScreen.tsx
+++ src/screens/Usuarios/FormUsuariosScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { View, TextInput, ScrollView, SafeAreaView } from 'react-native';
+import { View, TextInput, Image, ScrollView, SafeAreaView } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 import { useFormUsuarios } from '../../hooks/useFormUsuarios';
 import { RootStackUsuarioParams } from '../../navigator/UsuarioNavigator';
 import { BtnTouch } from '../../componets/BtnTouch';
@@ -24,6 +26,39 @@ export const FormUsuariosScreen = ( { navigation, route }: Props ) => {
         handleInputChange('image',usuario.image);
         handleInputChange('correo',usuario.correo);
     },[]);
+
+    const pickImage = async () => {
+
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4,3],
+          quality: 0.2,
+        });
+
+        (!result.canceled) && (() => {
+          convertImageToBase64( result.assets[0].uri );
+        })();
+
+    };
+
+    const convertImageToBase64 = async (imageUri: string) => {
+
+        try {
+
+          const base64 = await FileSystem.readAsStringAsync(imageUri, {
+            encoding: FileSystem.EncodingType.Base64,
+          });
+
+          handleInputChange('image',base64);
+
+        } catch (error) {
+
+          console.error('Error converting image to base64:', error);
+
+        }
+
+    };
 
     return(
         <SafeAreaView
@@ -65,23 +100,31 @@ export const FormUsuariosScreen = ( { navigation, route }: Props ) => {
                         />
                         <TextInput
                             style={ appTheme.input }
-                            value={ state.image }
-                            onChangeText={ (text) => handleInputChange('image', text) }
-                            placeholder='Imagen del usuario'
-                        />
-                        <TextInput
-                            style={ appTheme.input }
                             value={ state.password }
                             secureTextEntry={ true }
                             onChangeText={ (text) => handleInputChange('password', text) }
                             placeholder='Contraseña'
                         />
                         <BtnTouch
+                            title="Seleccionar Imagen" 
+                            action={ pickImage } 
+                            background='black'
+                        />
+                        {
+                            ( state.image !== '' ) && 
+                            (
+                                <Image 
+                                    source={{ uri: `data:image/jpeg;base64,${state.image}` }} 
+                                    style={{ width: 150, height: 150, borderRadius: 20 }} 
+                                />
+                            )
+                        }
+                        <BtnTouch
                             action={() => {
                                 handleSubmit();
                                 navigation.popToTop();
                             }}
-                            title={ ( state.id_user !== '' ) ? 'Actualizar Registro' : 'Crear registro' }
+                            title={ ( state.id_user !== undefined ) ? 'Actualizar Registro' : 'Crear registro' }
                             background='violet'
                         />
                         <BtnTouch
